4c4e5dfb64c99a7f436330e5ceed64a0d7691a74
MFMdeRooij <58552182+MFMdeRooij@users.noreply.github.com>
2019-12-10 15:40:19 +0100

    Add files via upload

---
+++ FastqToCountTable.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################################################################
+# We used the screen design of Jastrzebski et al Methods Mol Biol 2016
+# Open in Spyder, adjust the settings, and run the script
+# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2019, info: m.f.derooij@amsterdamumc.nl
+#####################################################################################################
+#                                            SETTINGS
+
+# Screen files
+screens = ("H:/BioWin/fastq/5306_1_RB_01_19_02_CGATGT_S1_R1_001.fastq.gz",) 
+
+library = "H:/BioWin/fastq/LibraryKinomeBrunelloLentiGuide.csv"
+
+# Barcodes (all the same lentgh) and allowed barcode mismaches and max indels allowed in upstream region (primers)
+barcode = ("CGTGAT", "ACATCG", "GCCTAA", "TGGTCA", "CACTGT", "ATTGGC", 
+		"GATCTG", "TCAAGT", "CTGATC", "AAGCTA", "GTAGCC", "TACAAG")
+BCmm,indel = 1,3
+
+# Library type: 1 = CRISPR, 2 = shRNA
+lib = 1
+if lib == 1: 
+    # Location CRISPR seq-1, length CRISPR, and upstream nucleotides (lentiCRISPRv2/LentiGuide: 41, 20, "CACCG")
+    loc,CRISPRsize,upseq  = 41,20,"CACCG"
+if lib == 2: 
+    # Location shRNA seq-1 and length shRNA,  and upstream nucleotides (pKLO.1 TRC1/1.5 and TRC2: 42, 21, "ACCGG")
+    loc,CRISPRsize,upseq = 42,21,"ACCGG"
+######################################################################################################
+# Make dictionary with barcode numbers
+BCnum = {}
+for i in range(len(barcode)):
+    BCnum[barcode[i]]=i+1
+
+# Make dictionary with barcode pieces 
+BCpieces = {}
+BCsize = len(barcode[0])
+index = 0
+for bc in barcode:
+  index+=1
+  for nt in range(BCsize):
+    BCpieces[str(index)+"."+str(nt+1)] = bc[nt]
+
+# Load library and make count table precursor
+libraryGuides,libraryGenes,librarySeqs = [],[],[]
+guides = open(library, 'r')
+line = guides.readline()
+while line:
+    listLine = line.split(",")
+    libraryGuides.append(listLine[0])
+    libraryGenes.append(listLine[1])
+    librarySeqs.append(listLine[2].strip("\n"))
+    line = guides.readline()
+guides.close()    
+# List of lists (multidimensional list)
+countTablePre = [libraryGuides, libraryGenes, librarySeqs]
+
+for screen in screens:
+    # Make multidimensional list with read sequences 
+    readTable = []
+    for i in range(len(barcode)):
+        readTable.append([])
+    
+    # Read Fastq file and write reads in the right barcode file
+    screenData = screen
+    import gzip
+    FASTQ = gzip.open(screenData, 'r')
+    while True:
+        name = FASTQ.readline()
+        if not name:
+            break
+        seq = FASTQ.readline()
+        plusline = FASTQ.readline()
+        qual = FASTQ.readline()
+        seq = seq.decode("utf-8")
+        
+        # Determine barcode
+        BCnumber = 0
+        BCreads = seq[0:BCsize]
+        if BCreads in barcode:
+            # When barcode has no mismatches
+            BCnumber = BCnum[BCreads]
+        else:
+            if BCmm > 0:
+                  BCread = {}
+                  for nt in range(BCsize):
+                      BCread["s"+str(nt+1)] = seq[nt]  
+            # Calculate similarities with barcode hash
+            for ibc in range(len(barcode)):
+                m1 = 0
+                for i in range(BCsize):
+                    if BCread["s"+str(i+1)] == BCpieces[str(ibc+1)+"."+str(i+1)]:
+                        m1+=1
+                if m1 >= BCsize-BCmm:
+                    BCnumber = ibc+1
+                    break
+    
+        # Determine CRISPR sequence
+        if BCnumber > 0:
+           seqtag = seq[loc:loc+CRISPRsize]
+           # When there is an indel in constant region, change CRISPR seq location 
+           if indel > 0:
+               upseqlength = len(upseq)
+               seqcheck = seq[loc-upseqlength:loc]   
+               if upseq != seqcheck: 
+                   for i in range(-indel,indel):
+                       preseq = seq[loc-upseqlength+i:loc-upseqlength+upseqlength+i]
+                       if preseq == upseq:
+                           seqtag = seq[loc+i:loc+CRISPRsize+i]
+                      
+           # Make multidimensional array with read sequences
+           readTable[BCnumber-1].append(seqtag)
+    FASTQ.close() 
+    
+    # Start new count table
+    readCounts = []
+    for i in range(len(barcode)):
+        readCounts.append([])
+        # Produce count tables per barcode and add to multidimensional list
+        for j in librarySeqs:
+            rc = readTable[i].count(j)
+            readCounts[i].append(rc)
+            # Name BCs
+        readCounts[i][0] = "BC"+str(i+1)
+    countTable = countTablePre + readCounts
+    transpose = [*zip(*countTable)]
+    
+    # Write count table to csv file
+    import csv
+    cell = screen.split("/") [-1]
+    myCsv = open("CountTable_"+cell+".csv", "w", newline='')
+    csvWriter = csv.writer(myCsv, delimiter=',')
+    csvWriter.writerows(transpose)
+    myCsv.close() 
---
+++ VolcanoSplit_GeneOfInterest_v1.0.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 13:46:40 2019
+
+@author: Martin F.M. de Rooij, PhD
+"""
+# Copy-paste the required folders from MA-viewer to your disk, Fill in the complete path (use / instead of \ ), 
+# and separate multiple folders by a comma, For example:
+'''
+files = ['H:/BioWin/01 Namalwa PMAcsv20190301173828', 
+         'H:/BioWin/02 Namalwa IgMcsv20190301173828 - Copy', 
+         'H:/BioWin/03 Namalwa IgM-PMAcsv20190301173828 - Copy (2)']
+'''
+###
+
+files = ['H:/BioWin/01 Namalwa PMA/' , 'H:/BioWin/02 Namalwa IgM/' , 'H:/BioWin/03 Namalwa IgM-PMA/', 'H:/BioWin/04 Namalwa Lethality/']
+
+# Which genes to highlight:
+gene = ['BTK', 'SYK', 'PIK3R1']
+
+# Save file as PDF (0=no, 1=yes)
+save = 1
+
+# Show significant genes as triangles (0=no, 1=yes)
+genesignificant = 1
+
+# Title:
+maintitle = 'NAMALWA'
+xtitle = '$^{2}$log median fold change (t$_1$/t$_0$)'
+
+#0 and 1 subscript: '$^{2}$log fold change (t$_1$/t$_0$)'
+#greek alpha letter: r'$^{2}$log fold change ($\alpha$IgM/PMA)'   ->include the upstream r for greek letters 
+
+# Font size of X- and Y-axis titles (the main title is 10 bigger)
+fs = 15
+
+# Colors (see https://matplotlib.org/1.5.1/examples/color/named_colors.html)
+call = 'green'
+cpos = 'firebrick'
+cneg = 'blue'
+chit = 'black'
+
+####################################################################################################
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import warnings
+warnings.filterwarnings('ignore')
+
+for file in files: 
+    nameGene = glob.glob(file+'*genes.csv')[0]           
+    dfg = pd.read_csv(nameGene, sep=',')
+    
+    dfg['GeneSymbol'] = dfg['GeneSymbol'].str.upper()
+    dfg['l2mfc'] = np.log2(dfg['MedianFoldChange'])
+    dfg['l10rdep'] = np.log10(dfg['rhoDepleted']+10**-15)*-1
+    dfg['l10renr'] = np.log10(dfg['rhoEnriched']+10**-15)*-1
+    dfg = dfg[dfg['Type']=='x'].append(dfg[dfg['Type']!='x'].sample(frac=1, random_state=10))
+ 
+    dfgene = dfg[dfg['GeneSymbol'].isin(gene)]
+    
+    fig, axs = plt.subplots(ncols=2, nrows=1, figsize=(10,5))
+    # remove the underlying axes
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.05)
+    gs = axs[0].get_gridspec()
+    axbig = fig.add_subplot(gs[0, 0:2])
+
+    ymin=-1
+    ymax=16
+    xmin=min(dfg['l2mfc']-0.5)
+    xmax=max(dfg['l2mfc']+0.5)
+                        
+    axbig.set_xticks([])
+    axbig.set_yticks([])
+    axbig.spines['bottom'].set_linewidth(0)
+    axbig.spines['top'].set_linewidth(0)
+    axbig.spines['left'].set_linewidth(0)
+    axbig.spines['right'].set_linewidth(0)
+    axbig.text(0.5, 1.1, maintitle, fontsize=fs+10, ha='center')
+    axbig.text(0.5, -0.25, xtitle, fontsize=fs, ha='center')
+    axbig.set_facecolor('None')
+
+    axs[0].set_xlim(xmin,xmax)
+    axs[0].set_ylim(ymin,ymax)
+    axs[0].set_ylabel(r'- $^{10}$log $\rho$ ($\alpha$RRA$_{depleted}$)', fontsize=fs)
+    axs[0].axvline(0, color='black', linewidth=1)  
+    axs[0].spines['bottom'].set_linewidth(1.5)
+    axs[0].spines['top'].set_linewidth(1.5)
+    axs[0].spines['left'].set_linewidth(1.5)
+    axs[0].spines['right'].set_linewidth(1.5)                            
+    for i, row in dfg.iterrows():    
+        label_pos_x = row['l2mfc']
+        label_pos_y = row['l10rdep']
+        # create and add the dots and text annotation to the scatterplot
+        axs[0].scatter(
+           label_pos_x,
+           label_pos_y,
+           color=cpos if row['Type']=='p' else  cneg if row['Type']=='n' else call,
+           marker = "v" if row['fdrDepleted'] < 0.1 and genesignificant==1 else "^" if row['fdrEnriched'] < 0.1 and genesignificant else "o",
+           s=25
+        )    
+
+    for i, row in dfgene.iterrows(): 
+        label = row['GeneSymbol']
+        label_pos_x = row['l2mfc']
+        label_pos_y = row['l10rdep']
+        # create and add the dots and text annotation to the scatterplot
+        axs[0].scatter(
+            label_pos_x,
+            label_pos_y,
+            color=chit,
+            marker = "v" if row['fdrDepleted'] < 0.1 and genesignificant==1 else "^" if row['fdrEnriched'] < 0.1 and genesignificant==1 else "o" ,
+            s=25
+        ) 
+        axs[0].text(
+            label_pos_x+0.01,
+            label_pos_y+0.3,
+            label,
+            color=chit,
+            rotation=10,
+            fontsize=fs
+        )
+
+
+    axs[1].yaxis.tick_right()
+    axs[1].yaxis.set_label_position('right')
+    axs[1].set_xlim(xmin,xmax)
+    axs[1].set_ylim(ymin,ymax)
+    axs[1].set_ylabel(r'- $^{10}$log $\rho$ ($\alpha$RRA$_{enriched}$)', rotation=-90, fontsize=fs, labelpad=20)
+    axs[1].axvline(0, color='black', linewidth=1)
+    axs[1].spines['bottom'].set_linewidth(1.5)
+    axs[1].spines['top'].set_linewidth(1.5)
+    axs[1].spines['left'].set_linewidth(1.5)
+    axs[1].spines['right'].set_linewidth(1.5) 
+    for i, row in dfg.iterrows():    
+            label_pos_x = row['l2mfc']
+            label_pos_y = row['l10renr']
+            # create and add the dots and text annotation to the scatterplot
+            axs[1].scatter(
+                label_pos_x,
+                label_pos_y,
+               color=cpos if row['Type']=='p' else  cneg if row['Type']=='n' else call,
+               marker = "v" if row['fdrDepleted'] < 0.1 and genesignificant==1 else "^" if row['fdrEnriched'] < 0.1 and genesignificant else "o",
+               s=25
+            )    
+    for i, row in dfgene.iterrows(): 
+        label = row['GeneSymbol']
+        label_pos_x = row['l2mfc']
+        label_pos_y = row['l10renr']
+        # create and add the dots and text annotation to the scatterplot
+        axs[1].scatter(
+            label_pos_x,
+            label_pos_y,
+            color=chit,
+            marker = "v" if row['fdrDepleted'] < 0.1 and genesignificant==1 else "^" if row['fdrEnriched'] < 0.1 and genesignificant==1 else "o",
+            s=25
+        ) 
+        axs[1].text(
+            label_pos_x+0.01,
+            label_pos_y+0.3,
+            label,
+            color=chit,
+            rotation=10,
+            fontsize=fs
+        )
+
+    if save==0:
+        plt.show()
+    elif save == 1:    
+        plt.savefig(file+'/Volcano'+maintitle+'.pdf', bbox_inches='tight', transparent=True)
+    plt.close()
