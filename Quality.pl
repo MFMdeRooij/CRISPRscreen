8533371979b7f7ecbfdc67e891f96e633975e0fc
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2020-05-13 20:43:06 +0200

    Update Quality.pl

--- Quality.pl
+++ Quality.pl
@@ -4,8 +4,9 @@ use strict;
 #####################################################################################################
 # We used the screen design of Jastrzebski et al Methods Mol Biol 2016
 # Linux: run in command line: ./Quality.pl data1.fastq.gz data2.fastq.gz
-# Windows: Install Strawberry Perl, Unpack fastq.gz file with 7zip, replace 'zcat' (line 45) for 'type',
-# and run in command prompt: perl Quality.pl data1.fastq data2.fastq
+# Windows: Install Strawberry Perl, Unpack fastq.gz file with 7zip, replace 'zcat' (line 46) for 'type',
+# and run in command prompt: perl Quality.pl data1.fastq data2.fastq (if you use tab to complete the filename
+# windows will add .\ before the filename (.\data1.fastq), this will give problems in naming the output file, so don't use that
 # After running this code (and FastqToCountTable.pl), go further with the R script (Quality.R)
 # Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2019, info: m.f.derooij@amsterdamumc.nl
 #####################################################################################################
