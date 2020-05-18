891e09ba8508f9f269944a2f6e4a8661606e9fab
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2020-05-18 15:05:54 +0200

    Add files via upload

--- FastqToCountTable_Quality.py
+++ FastqToCountTable_Quality.py
@@ -4,9 +4,9 @@
 # We used the screen design of Jastrzebski et al Methods Mol Biol 2016
 # Apart from anaconda3, install Biopython: command line/Windows Powershell: conda install biopython
 # Open in Spyder, adjust the settings, and run the script
-# To produce a count table keep the variables after line 47 on 0
-# To have a nice overview of the quality of your reads, run the script in Spyder for a few minutes,
-# and subsequently run the read quality overview lines (select line 281-299 and press F9)
+# To produce count tables keep the 4 variables after line 47 on 0
+# To have a nice overview of the quality of your reads, keep the 4 variables after line 47 on 1, run the script in Spyder
+# for a few minutes, and subsequently run the read quality overview lines (select line 307-327 and press F9)
 # Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2020, info: m.f.derooij@amsterdamumc.nl
 #####################################################################################################
 # Import python modules
@@ -22,7 +22,7 @@ from Bio import pairwise2
 # Workdirectory
 os.chdir("H:/BioWin/Screens/")
 
-# Screen files
+# FASTQ files
 screens = ("test.fastq.gz",) 
 
 # Library file
@@ -44,22 +44,29 @@ barcode = ["CGTGAT", "ACATCG", "GCCTAA", "TGGTCA", "CACTGT", "ATTGGC",
 # Number of allowed mismaches in barcodes, and max indels in constant part of the reads (PCR-primer mutations) (default = 1,3)
 BCmm,indel = 1,3 # for barcodes of 6 nucleotides do not use more than 1 mismatch (to see multiple matches comment out 'break' in line 105, and use printUnID = 1 in line 50)
 
-# FOR QUICK MAPPING KEEP NEXT VARIABLES ON 0
+# FOR FAST MAPPING KEEP NEXT 4 VARIABLES ON 0
 
-# Print information about the barcode and guide sequences: 0 = No, 1 = yes
+# Count and print information about the identification of barcodes and guides: 0 = No, 1 = yes
 printUnID = 1
+# From line 115 and 200 you can comment out any block if you want to skip it, especially biopython's alignment is very slow
 
-# Use regex and Biopython's pairwise2 alignment for barcodes and guides identification: 0 = No, 1 = yes (for quality checks only)
+# Use regex and Biopython's pairwise2 alignments for barcodes and guides identification: 0 = No, 1 = yes
 biopythonBarcode = 1
-biopythonGuide = 0 # (Keep in mind that mutations in the guide sequence could be non-functional or aspecific)
+biopythonGuide = 1 # (Keep in mind that mutations in the guide sequence could be non-functional or aspecific)
 
 # Remove unidentified counts from count table (recommended for further analysis) 0 = yes, 1 = no !!
-RemoveUnID = 1 
-######################################################################################################
-    
+RemoveUnID = 1
+
+# Reverse-complement of the template part of the reverse primer 
+revCompRevPrimerSeq = "GGAATTGGCTCCGGTGCCCGTCAGT"
+# Primerdimer (dependent on primer design, high fidelity polymerases have proofreading (exonuclease activity), in which not nessesary 
+# the 3'ends have to be complementary, so remove the first few nucleotides if no primerdimers can be found)
+
+# Downstream nucleotides after guide sequence (CRISPR: "GTT", shRNA (TRC): "CT")
+downseq = "GTT"
+######################################################################################################   
 # Make dictionary with barcode numbers
 BCnum = dict(zip(barcode, np.arange(12)+1))
-
 # Make dictionary with barcode pieces 
 BCpieces = {}
 BCsize = len(barcode[0])
@@ -67,13 +74,13 @@ for index, bc in enumerate(barcode, start=1):
   for nt in range(BCsize):
     BCpieces[str(index)+"."+str(nt+1)] = bc[nt]
 
-# Load library and make count table precursor
+# Load library
 Library = pd.read_csv(library)
 Library = pd.DataFrame([["Unidentified"]*len(Library.columns)], columns=Library.columns.tolist()).append(Library,ignore_index=True)
 LibrarySeq = Library["Sequence"].to_numpy()
 
 def barcodeDetermination(seq):
-    # Determine barcode
+    '''Identify barcodes'''
     BCreads = seq[0:BCsize]
     if printUnID == 1:
         global BCcorrect, BCmismatch, BCdel, BCins, BCalign
@@ -104,22 +111,25 @@ def barcodeDetermination(seq):
                         print("Barcodes identified with max %d mismatch(es): %d" % (BCmm,BCmismatch))
                     break  
                          
-            if biopythonBarcode == 1 and BCnumber == 0:
-                # Deletion
-                BCreads = seq[0:BCsize-1]
-                matchSeq = []
-                for i in range(len(BCreads)+1):
-                    pattern = re.compile(r''+str(BCreads[0:i])+'.'+str(BCreads[i:len(BCreads)]))
-                    for bc in barcode:   
-                        matches = pattern.findall(bc)
-                        for match in matches:
-                            matchSeq.append(bc)
-                if len(matchSeq) != 0:
-                    BCnumber = BCnum[max(set(matchSeq), key=matchSeq.count)]
-                    if printUnID == 1:
-                        print("This barcode (%s --> %s) is found by regex" % (BCreads, barcode[BCnumber-1]))          
-                        BCdel+=1
-                        print("Barcodes identified by regex (a deletion): %d" % (BCdel))  
+            if biopythonBarcode == 1:
+            # You can comment out any block if you want to skip it
+            
+                if BCnumber == 0:
+                    # Deletion
+                    BCreads = seq[0:BCsize-1]
+                    matchSeq = []
+                    for i in range(len(BCreads)+1):
+                        pattern = re.compile(r''+str(BCreads[0:i])+'.'+str(BCreads[i:len(BCreads)]))
+                        for bc in barcode:   
+                            matches = pattern.findall(bc)
+                            for match in matches:
+                                matchSeq.append(bc)
+                    if len(matchSeq) != 0:
+                        BCnumber = BCnum[max(set(matchSeq), key=matchSeq.count)]
+                        if printUnID == 1:
+                            print("This barcode (%s --> %s) is found by regex" % (BCreads, barcode[BCnumber-1]))          
+                            BCdel+=1
+                            print("Barcodes identified by regex (a deletion): %d" % (BCdel))  
 
                 if BCnumber == 0:
                     # Insertion
@@ -157,14 +167,15 @@ def barcodeDetermination(seq):
     return(BCnumber)
    
 def guideDetermination(seq):
-    guide = seq[loc:loc+CRISPRsize]
+    '''Identify guides'''
+    guide = seq[loc:loc+CRISPRsize]   
     if biopythonGuide == 1:
         guideForInsert = seq[loc:loc+CRISPRsize+1]
     if printUnID == 1:
-        global total, correct, indels, mismatch, deletion, insertion, align 
+        global total, correct, indels, mismatch, deletion, insertion, align, dimer, otherGuide 
         total+=1
         print("\nTotal reads analyzed: %d" % (total))
-    # When there is an indel in constant region, change CRISPR seq location 
+    # When there is an indel in constant region, change CRISPR seq location
     if indel > 0:
         upseqlength = len(upseq)
         seqcheck = seq[loc-upseqlength:loc]   
@@ -179,29 +190,31 @@ def guideDetermination(seq):
                         print("This read (%s) has %d indel" % (seq, i))
                         indels+=1
                         print("Reads with indels of max %dnt in upstream region of the guide: %d" % (indel,indels))
-                    break
-                    
+                    break                 
     guideNumber=np.where(LibrarySeq==guide)[0]
     if printUnID == 1 and guideNumber.size != 0:
         correct+=1
         print("Correct guides: %d" % (correct))  
         
-    if biopythonGuide == 1 and guideNumber.size == 0: 
-        # Deletion
-        Guidereads = guide[0:CRISPRsize-1]
-        matchSeq = []
-        for i in range(len(Guidereads)+1):
-            pattern = re.compile(r''+str(Guidereads[0:i])+'.'+str(Guidereads[i:len(Guidereads)]))
-            for seqG in LibrarySeq:   
-                matches = pattern.findall(seqG)
-                for match in matches:
-                    matchSeq.append(seqG)
-        if len(matchSeq) != 0:
-            guideNumber=np.where(LibrarySeq==max(set(matchSeq), key=matchSeq.count))[0]  
-            if printUnID == 1:
-                print("This guide (%s --> %s) is found by regex" % (Guidereads, LibrarySeq[guideNumber]))          
-                deletion+=1
-                print("Guides identified by regex (a deletion): %d" % (deletion))  
+    if biopythonGuide == 1:
+    # You can comment out any block if you want to skip it
+    
+        if guideNumber.size == 0: 
+            # Deletion
+            Guidereads = guide[0:CRISPRsize-1]
+            matchSeq = []
+            for i in range(len(Guidereads)+1):
+                pattern = re.compile(r''+str(Guidereads[0:i])+'.'+str(Guidereads[i:len(Guidereads)]))
+                for seqG in LibrarySeq:   
+                    matches = pattern.findall(seqG)
+                    for match in matches:
+                        matchSeq.append(seqG)
+            if len(matchSeq) != 0:
+                guideNumber=np.where(LibrarySeq==max(set(matchSeq), key=matchSeq.count))[0]  
+                if printUnID == 1:
+                    print("This guide (%s --> %s) is found by regex" % (Guidereads, LibrarySeq[guideNumber]))          
+                    deletion+=1
+                    print("Guides identified by regex (a deletion): %d" % (deletion))  
 
         if guideNumber.size == 0:         
             # Mismatch
@@ -231,7 +244,7 @@ def guideDetermination(seq):
                     for match in matches:
                         matchSeq.append(seqG)
             if len(matchSeq) != 0:
-                guideNumber=np.where(LibrarySeq==max(set(matchSeq), key=matchSeq.count))[0]    
+                guideNumber=np.where(LibrarySeq==max(set(matchSeq), key=matchSeq.count))[0]   
                 if printUnID == 1:
                     print("This guide (%s --> %s) is found by regex" % (Guidereads, LibrarySeq[guideNumber]))          
                     insertion+=1
@@ -251,19 +264,32 @@ def guideDetermination(seq):
                     align+=1
                     print("Guides identified by biopython alignment (more indels and/or mismatches): %d" % (align))  
 
+        if guideNumber.size == 0 and printUnID == 1: 
+            # primerdimer or other CRISPR guides
+            Guidereads = seq
+            if re.search(str(revCompRevPrimerSeq), Guidereads):
+                print("This read is a primerdimer: %s" % (Guidereads))
+                dimer+=1
+                print("Primerdimers: %d" % (dimer)) 
+            else:
+                pattern = re.compile(r''+upseq+'(\w{'+str(CRISPRsize)+'})'+str(downseq))
+                match = pattern.search(Guidereads)
+                if match != None:
+                    print("This guide (%s --> %s) is found by regex, but cannot be found in the library" % (Guidereads, match.group(1)))          
+                    otherGuide+=1
+                    print("Unidentified guides (contamination?): %d" % (otherGuide)) 
+
     if guideNumber.size == 0:
         guideNumber = 0       
         if printUnID == 1:    
-            print(seq+"This guide cannot be identified: "+guide)
-    
+            print(seq+"This guide cannot be identified: "+guide)   
     return guideNumber
 
 # Count the reads
 for screen in screens:
     if printUnID == 1:
-        total=correct=indels=mismatch=deletion=insertion=align = 0
+        total=correct=indels=mismatch=deletion=insertion=align=dimer=otherGuide = 0
         BCcorrect=BCmismatch=BCdel=BCins=BCalign = 0
-
     countTableToFillZero = np.zeros((len(Library), len(barcode)+1))
     # Read Fastq file and put counts in the count table
     screenData = screen
@@ -290,12 +316,14 @@ for screen in screens:
             print("Barcodes identified by biopython aligment (more mutations): %d" % (BCalign))
         if indel > 0:
             print("Reads with indels of max %dnt in upstream region of the guide: %d" % (indel,indels))
-        print("Correct guides: %d" % (correct))  
+            print("Primerdimers: %d" % (dimer))  
         if biopythonGuide == 1:
+            print("Correct guides: %d" % (correct)) 
             print("Guides identified by regex (a mismatch): %d" % (mismatch))
             print("Guides identified by regex (a deletion): %d" % (deletion))  
             print("Guides identified by regex (an insertion): %d" % (insertion))  
-            print("Guides identified by biopython alignment (more indels and/or mismatches): %d" % (align)) 
+            print("Guides identified by biopython alignment (more indels and/or mismatches): %d" % (align))        
+            print("Unidentified guides (contamination?): %d" % (otherGuide)) 
         print("Check also the 'countTableFilled' table in the next step, to see the distribution of unidentified counts per barcode/guide")
             
     # Write count table to file
