705c88cc338873f9cd89f1be026082a3d3f3c901
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2021-05-12 19:08:50 +0200

    Update GeneExpressionRNAseqHisat2.R

--- RNAseq/GeneExpressionRNAseqHisat2.R
+++ RNAseq/GeneExpressionRNAseqHisat2.R
@@ -1,17 +1,21 @@
+# Follow the lines with hastag signs
+# Run single-end and paired-end data separately
+# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2019, info: m.f.derooij@amsterdamumc.nl
+
 # Workdirectory
-Workdirectory<- "~/Data/RNAseqCellLines/"
+Workdirectory <- "~/Data/RNAseqCellLines/"
+
+# Paired-end reads 0 = Yes, 1 = No
+pairedEnd <-0
 
 # Remove SRA and intermediate FASTQ files 0 = Yes, 1 = No
-removeSRA<- 0
-
-# Cells which are paired-end sequenced)
-pairedEnd<- c("SRR1031032_MINO.sra", "SRR8615896_U266.sra", "SRR8616164_X697.sra", "SRR7611497_BJAB.sra","SRR7659713_UPN1.sra")
+removeSRA <- 0
 
 # Order count table
 q <- function(...) {
   sapply(match.call()[-1], deparse)
 }
-Sort<- q(ensembl_gene_id,	hgnc_symbol,	NALM6, REH, SEM, X697, MEC1,	GRANTA519,	JEKO1,	MINO,	REC1, Z138, DAUDI, NAMALWA,	RAJI, DOHH2, SUDHL4, SUDHL5, 
+Sort <- q(ensembl_gene_id,	hgnc_symbol,	NALM6, REH, SEM, X697, MEC1,	GRANTA519,	JEKO1,	MINO,	REC1, Z138, DAUDI, NAMALWA,	RAJI, DOHH2, SUDHL4, SUDHL5, 
                 SUDHL6,	OCILY3,	OCILY10,	TMD8,	U2932, EJM, INA6, L363,	LP1, MM1S, NCIH929,	OPM2,	RPMI8226, U266)
 ##########################################
 setwd(Workdirectory)
@@ -22,8 +26,7 @@ setwd(Workdirectory)
 ##Linux Tools:
 #sudo apt-get install seqtk
 #sudo apt-get install samtools
-#sudo apt-get install bowtie2
-#sudo apt install hisat2
+#sudo apt-get install hisat2
 
 #biomaRt dependencies:
 #sudo apt-get install libcurl4-openssl-dev libssl-dev
@@ -35,7 +38,7 @@ setwd(Workdirectory)
 #setwd(Workdirectory)
 
 ##Download reference files for RNAseq from NCBI or UCSC:
-# File with gene loci (hg38.95.gtf)
+# Download File with gene loci (hg38.95.gtf)
 # Transform the gtf file into a known splice site text file using the python script delivered with hisat2 (to hg38_splicesites.txt)
 
 ##R Packages
@@ -46,89 +49,34 @@ library(Rsubread)
 library(biomaRt)
 library(DESeq2)
 
-##Download SRR files:
-##https://www.ncbi.nlm.nih.gov/sra/
-##On command line (SRA tools):
-#prefetch SRR6047361
-##for multiple SRR files:
-#for i in {2..4}; do prefetch SRR604736$i; done
-
-##To tranfer files to another location
-#for i in *.sra; do sudo cp $i ~/SharedFolder/$1; done
-
-
-# Convert SRA files to FastQ files
-files <- (Sys.glob("*.sra"))
-for (cell in files) {
-  system(paste0("fastq-dump -v ",cell))
-  #"fastq-dump --split-files FILE": for immediately splitting paired-end fastq files
-  if (removeSRA == 0) {
-    file.remove(cell)
-  }
+# Search for RNAseq data on https://www.ncbi.nlm.nih.gov/sra/, and fill in the SRR codes in MapSamples.txt
+if (pairedEnd==0){
+  system("./MapRNAseqHuman_SRA_PE.sh")
+}
+if (pairedEnd==1){
+  system("./MapRNAseqHuman_SRA_U.sh")
 }
 
-# Count read length
-system("perl GeneExpressionRNAseqReadSize.pl")
-df_readLength<- read.csv('ReadSize.csv', stringsAsFactors = F)
-
-# Split paired-end FASTQ files
-pairedEndfastq<-paste0(pairedEnd, '.fastq')
-for (cell in pairedEndfastq) {
-    numberCutOff<-df_readLength[df_readLength$cell==cell,"readLength"]/2 
-    # -e remove nt for the end, -b remove nt from begin
-    system(paste0("seqtk trimfq -e ", numberCutOff, " ", cell, " > 1.", cell))
-    system(paste0("seqtk trimfq -b ", numberCutOff, " ", cell, " > 2.", cell))
-    if (removeSRA == 0) {
-      file.remove(cell)
-    }
-}    
-
-# Trim FASTQ files (to remove low quality sequences)
-files <- (Sys.glob("*.sra.fastq"))
-for (cell in files) {    
-    system(paste0("seqtk trimfq ", cell, " > ", cell, ".fq"))
-    if (removeSRA == 0) {
-      file.remove(cell)
-    }   
-} 
-
-# Run hisat2 (mapping reads to genome)
-
-# first paired-ends
-for (PE in pairedEnd){
-  dir.create(paste0(PE,".fastq.fq1"))
-  file.rename(paste0("1.",PE,".fastq.fq"), paste0(PE,".fastq.fq1/1.",PE,".fastq.fq"))
-  file.rename(paste0("2.",PE,".fastq.fq"), paste0(PE,".fastq.fq1/2.",PE, ".fastq.fq"))
-  setwd(paste0(PE,".fastq.fq1"))
-  system(paste0("hisat2 -x ~/HumanGenome/hg38 --known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt -1 1.", PE, ".fastq.fq -2 2.", PE, ".fastq.fq -S ", PE, ".fastq.fq.sam --summary-file ", PE, ".fastq.fq.Summary.txt")) 
-  file.rename(paste0(PE,".fastq.fq.sam"), paste0("../",PE,".fastq.fq.sam"))
-  setwd(Workdirectory)
-}
-
-# Then the single-end
-files <- (Sys.glob("*.fq"))
-for (cell in files) {
-  dir.create(paste0(cell,1))
-  file.rename(cell,paste0(cell,1,"/",cell))
-  setwd(paste0(cell,1))
-  system(paste0("hisat2 -x ~/HumanGenome/hg38 --known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt -U ", cell, " -S ", cell, ".sam --summary-file ", cell, ".Summary.txt")) 
-  file.rename(paste0(cell,".sam"), paste0("../",cell,".sam"))
-  setwd(Workdirectory)
-}
+# Add an underscore and a cell name (which corresponds to a cell name in the Sort vector) to the SAM filenames ("SRR1031032.sam" -> "SRR1031032_MINO.sam")
 
 # Make Count table (TPM with DESEq2 normalisation to make it comparable between samples)
-fc <- featureCounts(files=Sys.glob("*.sam"), 
-                    annot.ext="~/HumanGenome/hg38.95.gtf", 
-                    isGTFAnnotationFile=TRUE,
-                    isPairedEnd=FALSE,
-                    strandSpecific=0)
-
+if (pairedEnd==0){
+  fc <- featureCounts(files=Sys.glob("*.sam"), 
+                      annot.ext="~/HumanGenome/hg38.95.gtf", 
+                      isGTFAnnotationFile=TRUE,
+                      isPairedEnd=TRUE,
+                      strandSpecific=0)
+}
+if (pairedEnd==1){
+  fc <- featureCounts(files=Sys.glob("*.sam"), 
+                      annot.ext="~/HumanGenome/hg38.95.gtf", 
+                      isGTFAnnotationFile=TRUE,
+                      isPairedEnd=FALSE,
+                      strandSpecific=0)
+}
 df_annotation<-as.data.frame(fc[2])
-
 CountTable3<- as.data.frame(fc[1])
-colnames(CountTable3)<- unlist(lapply(strsplit(colnames(CountTable3), ".sra"), "[[", 1))
 colnames(CountTable3)<- unlist(lapply(strsplit(colnames(CountTable3), "_"), "[[", 2))
-
 CountTable2<- CountTable3/(df_annotation$annotation.Length)
 CountTable<- t(t(CountTable2)/colSums(CountTable2)*1000000)
 CountTable<- as.data.frame(CountTable)
@@ -142,11 +90,8 @@ CountTable$ensembl_gene_id<- row.names(CountTable)
 # 
 # write.csv(GeneList, "GeneList.csv", row.names = F)
 GeneList<-read.csv('GeneList.csv', stringsAsFactors = F)
-
 CountTable<- merge(GeneList, CountTable, by="ensembl_gene_id", all.x=TRUE)
 CountTable$description<-NULL
-
-
 write.csv(CountTable, "RNAseqCountTable2.csv", row.names=FALSE)
 
 # Add new cell lines to an existing table
@@ -158,10 +103,9 @@ write.csv(CountTable, "RNAseqCountTable.csv", row.names=FALSE)
 sf <- estimateSizeFactorsForMatrix(CountTable[,3:ncol(CountTable)])
 CountTableNor<-CountTable
 CountTableNor[,3:ncol(CountTable)]<- as.data.frame(round(t(t(CountTable[,3:ncol(CountTable)])/sf),1))
-
 CountTableNor<-CountTableNor[,Sort]
-
 write.csv(CountTableNor, "RNAseqCountTableNorTPM.csv", row.names=FALSE)
+
 
 #PCA analysis
 #install.packages(c("corrplot", "ggplot2", "ggfortify"), dependencies=T)
