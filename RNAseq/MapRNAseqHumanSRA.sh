34c6f96856d5f8aa2ca1ceae2c273eecf84c030b
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2021-05-14 16:29:19 +0200

    Update MapRNAseqHumanSRA.sh

--- RNAseq/MapRNAseqHumanSRA.sh
+++ RNAseq/MapRNAseqHumanSRA.sh
@@ -6,7 +6,7 @@
 # The first time, make the bash script executable (chmod 755 MapRNAseqHumanSRA.sh)
 # Afterwards you can view the mapping in IGV viever, or make a count table with the R
 # script (GeneExpressionRNAseqHisat2)
-# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2019,
+# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2021,
 # info: m.f.derooij@amsterdamumc.nl
 mkdir pairedEnd
 mkdir singleEnd
@@ -28,7 +28,7 @@ do
 	 	-1 ${s}_1.fq -2 ${s}_2.fq -S ${s}_${c}.sam\
 	 	--summary-file ${s}_${c}.summmary.txt
 	 	samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
-	 	mv ${s}_${c}.sam pairedEnd/${s}_${c}.sam
+	 	mv ${s}_${c}.sam pairedEnd/
 		rm ${s}_1.fastq
 		rm ${s}_2.fastq
 		rm ${s}_1.fq 
@@ -40,11 +40,12 @@ do
 		--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
 	 	-U $s.fq -S ${s}_${c}.sam --summary-file ${s}_${c}.summmary.txt
 	 	samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
-	 	mv ${s}_${c}.sam singleEnd/${s}_${c}.sam
+	 	mv ${s}_${c}.sam singleEnd/
 		rm $s.fastq 
 		rm $s.fq 
 	fi
 	samtools sort ${s}_${c}.bam > ${s}_${c}.sort.bam
 	samtools index ${s}_${c}.sort.bam
 	rm ${s}_${c}.bam
+	rm $s.sra
 done
