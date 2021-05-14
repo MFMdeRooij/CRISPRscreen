bf0a4451127fd0874933c6b44d0c586e96a68d6a
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2021-05-14 10:26:08 +0200

    Add files via upload

---
+++ RNAseq/MapRNAseqHumanSRA.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Search for RNAseq data on https://www.ncbi.nlm.nih.gov/sra/, and fill in the SRR codes, cell # line ID and P (paired-end or U (unpaired) separated by commas in MapSamples.txt, and run the # script (./MapRNAseqHumanSRA on the command line)
+# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2019,
+# info: m.f.derooij@amsterdamumc.nl
+for line in `cat MapSamples.txt`
+do 
+	s="$(echo $line | cut -d"," -f1)"
+	c="$(echo $line | cut -d"," -f2)"
+	p="$(echo $line | cut -d"," -f3)"
+	echo ID:$s Cell:$c Reads:$p
+	prefetch $s
+	mv ~/ncbi/sra/$s.sra $PWD 
+	if [ $p = P ] 
+	then
+		fastq-dump -v --split-files $s.sra
+		seqtk trimfq ${s}_1.fastq > ${s}_1.fq
+		seqtk trimfq ${s}_2.fastq > ${s}_2.fq
+		hisat2 -x ~/HumanGenome/hg38\
+		--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
+	 	-1 ${s}_1.fq -2 ${s}_2.fq -S ${s}_${c}.sam\
+	 	--summary-file ${s}_${c}.summmary.txt
+		rm ${s}_1.fastq
+		rm ${s}_2.fastq
+		rm ${s}_1.fq 
+		rm ${s}_2.fq
+	else
+		fastq-dump -v $s.sra
+		seqtk trimfq $s.fastq > $s.fq
+		hisat2 -x ~/HumanGenome/hg38\
+		--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
+	 	-U $s.fq -S ${s}_${c}.sam --summary-file ${s}_${c}.summmary.txt
+		rm $s.fastq 
+		rm $s.fq 
+	fi
+	samtools view -S -b $${s}_${c}.sam > ${s}_${c}.bam
+	samtools sort ${s}_${c}.bam > ${s}_${c}.sort.bam
+	samtools index ${s}_${c}.sort.bam
+	rm ${s}_${c}.bam
+done
