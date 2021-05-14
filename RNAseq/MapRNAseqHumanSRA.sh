78bf8275a78bac20a15bbe79acd05462c457838b
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2021-05-14 20:53:24 +0200

    Update MapRNAseqHumanSRA.sh

--- RNAseq/MapRNAseqHumanSRA.sh
+++ RNAseq/MapRNAseqHumanSRA.sh
@@ -18,32 +18,41 @@ do
 	echo ID:$s Cell:$c Reads:$p
 	prefetch $s
 	mv ~/ncbi/sra/$s.sra $PWD 
-	if [ $p = P ] 
-	then
-		fastq-dump -v --split-files $s.sra
-		seqtk trimfq ${s}_1.fastq > ${s}_1.fq
-		seqtk trimfq ${s}_2.fastq > ${s}_2.fq
-		hisat2 -x ~/HumanGenome/hg38\
-		--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
-	 	-1 ${s}_1.fq -2 ${s}_2.fq -S ${s}_${c}.sam\
-	 	--summary-file ${s}_${c}.summmary.txt
-	 	samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
-	 	mv ${s}_${c}.sam pairedEnd/
-		rm ${s}_1.fastq
-		rm ${s}_2.fastq
-		rm ${s}_1.fq 
-		rm ${s}_2.fq
-	else
-		fastq-dump -v $s.sra
-		seqtk trimfq $s.fastq > $s.fq
-		hisat2 -x ~/HumanGenome/hg38\
-		--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
-	 	-U $s.fq -S ${s}_${c}.sam --summary-file ${s}_${c}.summmary.txt
-	 	samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
-	 	mv ${s}_${c}.sam singleEnd/
-		rm $s.fastq 
-		rm $s.fq 
-	fi
+	while : 
+	do
+		if [ $p = P ] 
+		then
+			fastq-dump -v --split-files $s.sra
+			seqtk trimfq ${s}_1.fastq > ${s}_1.fq
+			seqtk trimfq ${s}_2.fastq > ${s}_2.fq
+			hisat2 -x ~/HumanGenome/hg38\
+			--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
+			-1 ${s}_1.fq -2 ${s}_2.fq -S ${s}_${c}.sam\
+			--summary-file ${s}_${c}.summmary.txt
+			samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
+			mv ${s}_${c}.sam pairedEnd/
+			rm ${s}_1.fastq
+			rm ${s}_2.fastq
+			rm ${s}_1.fq 
+			rm ${s}_2.fq
+			break
+		elif [ $p = P ] 
+		then
+			fastq-dump -v $s.sra
+			seqtk trimfq $s.fastq > $s.fq
+			hisat2 -x ~/HumanGenome/hg38\
+			--known-splicesite-infile ~/HumanGenome/hg38_splicesites.txt\
+			-U $s.fq -S ${s}_${c}.sam --summary-file ${s}_${c}.summmary.txt
+			samtools view -S -b ${s}_${c}.sam > ${s}_${c}.bam
+			mv ${s}_${c}.sam singleEnd/
+			rm $s.fastq 
+			rm $s.fq 
+			break
+		else
+			echo What means $p?, Is $s paired-end \(P\) or single-end \(S\)?
+			read p
+		fi
+	done
 	samtools sort ${s}_${c}.bam > ${s}_${c}.sort.bam
 	samtools index ${s}_${c}.sort.bam
 	rm ${s}_${c}.bam
