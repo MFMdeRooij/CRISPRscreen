41af0c2dc725d64f87b9c1bc4a93a0705960c357
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2021-05-18 16:36:06 +0200

    Update SyntheticLethality_GenesOfInterest.R

--- CRISPRScreenPlots/SyntheticLethality_GenesOfInterest.R
+++ CRISPRScreenPlots/SyntheticLethality_GenesOfInterest.R
@@ -1,7 +1,7 @@
 # Use the CRISPRScreenAnalysis.R output files of a synthetic lethality screen, adjust the settings, and run the script in R studio.
 # This script normalizes the median log2 fold change to the essential and non-essential genes of a synthetic lethality screen, and plots T1control/T0 against T1treated/T0. 
 # This normalization can improve the comparison treated - control if the treated arm did not have equal cell divisions, however the separation between the essentials and 
-# non-essentials will not be improved. Synthetic lethal genes will be on the lower half of the vertical 0 axis.
+# non-essentials will not be improved. Synthetic lethal genes will be located around the lower half of the vertical 0 axis.
 # Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2021, info: m.f.derooij@amsterdamumc.nl
 ##################################################################################################################################
 #                                                                 SETTINGS
