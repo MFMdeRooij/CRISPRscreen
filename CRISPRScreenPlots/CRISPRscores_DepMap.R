e3a65bc36a7f393a8ad5aca8e24d7fa0688f7135
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2023-02-14 13:32:06 +0100

    Add files via upload

--- CRISPRScreenPlots/CRISPRscores_DepMap.R
+++ CRISPRScreenPlots/CRISPRscores_DepMap.R
@@ -1,7 +1,7 @@
 # Plot CRISPR scores of the public CRISPR screens from DepMap.org (Broad Institute)
-# Download "CRISPR (DepMap 22Q2 Public+Score, Chronos)" from https://depmap.org/portal/download/custom/, and include "Add cell line metadata to download"
-# All cell lines are plotted on page 1, and the hematological ones are plotted including labels on page 2
-# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2022, info: m.f.derooij@amsterdamumc.nl
+# Download CRISPR data (22q4) "CRISPRGeneEffect.csv, and model.csv from https://depmap.org/portal/download/all/
+# All cell lines are plotted on page 1, and the lymphoid ones are plotted including labels on page 2
+# Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2023, info: m.f.derooij@amsterdamumc.nl
 ##################################################################################
 # Workdirectory (folder where "CRISPR_(DepMap_22Q2_Public+Score,_Chronos).csv" is located)
 setwd("H:/BioWin/DepMap/")
@@ -11,7 +11,7 @@ q <- function(...) {
   sapply(match.call()[-1], deparse)
 }
 interestingGenes <- toupper(q(
-# Which genes?  
+  # Which genes?  
   BTK, PIK3CD, CD79A, BCL6, PRDM1
 ))
 ##################################################################################
@@ -24,49 +24,54 @@ library("ggrepel")
 #devtools::install_github("JosephCrispell/basicPlotteR")
 library("basicPlotteR")
 if (!exists("dataDepMap")) {
-  dataDepMap<-read.csv("CRISPR_(DepMap_22Q2_Public+Score,_Chronos).csv",stringsAsFactors = F)
-  chronos<-as.data.frame(t(dataDepMap[,c(7:ncol(dataDepMap))]))
-  colnames(chronos)<- dataDepMap$cell_line_display_name
-  cells<- dataDepMap[,c(2,1,3:6)]
-  cellsHemato <- cells$cell_line_display_name[cells$lineage_1 %in% c("Blood", "Lymphocyte", "Plasma Cell")]
+  dataDepMap<-read.csv("CRISPRGeneEffect.csv",stringsAsFactors = F)
+  chronos<-as.data.frame(t(dataDepMap[,2:ncol(dataDepMap)]))
+  colnames(chronos)<- dataDepMap$X
+  rownames(chronos)<- unlist(lapply(strsplit(as.character(rownames(chronos)),"\\.\\."), "[", 1))
+  cells<- read.csv("Model.csv",stringsAsFactors = F)
+  UsedIDs<- cells[match(colnames(chronos), cells$ModelID),]
+  colnames(chronos)<- UsedIDs$StrippedCellLineName
+  
+  # Sample metadata
+  df_designAll<-cells[cells$StrippedCellLineName %in% UsedIDs$StrippedCellLineName,c("StrippedCellLineName","OncotreeLineage", "OncotreeSubtype")]
+  colnames(df_designAll)<- c("Samples", "Group", "Rep")
+  df_designAll$Rep[df_designAll$Rep==""]<-"Unspecified"
+  df_designAll<-df_designAll[order(df_designAll$Rep, decreasing = T),]
+  df_designAll<-df_designAll[order(df_designAll$Group, decreasing = T),]
+  
+  # Lymphoid selection
+  cellsHemato <- cells$StrippedCellLineName[cells$OncotreeLineage %in% c("Lymphoid")]
+  df_designHemato<- df_designAll[df_designAll$Samples %in% cellsHemato,]
 }
-
-# Sample metadata
-df_designAll<-cells[,c(1,3,4)]
-colnames(df_designAll)<- c("Samples", "Group", "Rep")
-df_designAll$Rep[df_designAll$Rep==""]<-"Unspecified"
-df_designAll<-df_designAll[order(df_designAll$Rep, decreasing = T),]
-df_designAll<-df_designAll[order(df_designAll$Group, decreasing = T),]
-df_designHemato<- df_designAll[df_designAll$Group %in% c("Plasma Cell", "Lymphocyte", "Blood"),]
 
 # Generate plots
 interestingGenes<-toupper(interestingGenes)
 for (gene in interestingGenes){
   # data table
   if (gene %in% rownames(chronos)){
-    pdf(paste0("CRISPRscores_",gene,".pdf"), width=10, height=20)
-      i<-1
-      for (df_design in list(df_designAll, df_designHemato)){
-        df_gene<- chronos[rownames(chronos)==gene,df_design$Sample]
-        df_gene<-as.data.frame(t(df_gene))
-        colnames(df_gene)<-"Gene"
-        df_gene$Group<-factor(df_design$Group, levels=unique(df_design$Group))
-        df_gene$color<- "black"
-        df_gene$Rep<-factor(df_design$Rep, levels=unique(df_design$Rep))
-        df_gene$Group<-interaction(df_gene$Group,df_gene$Rep, lex.order = T)
-    
-        # Barplot
-        g<-ggplot(df_gene, aes(x=Gene, y=Group, fill=Group, label=rownames(df_gene))) + geom_boxplot() + geom_jitter(color=df_gene$color, size=2, alpha=0.9, height = 0) + 
-          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + labs(y = "", x="CRISPR score (Chronos)") +
-          ggtitle(paste0(gene, " knockout (DepMap)")) + theme(legend.position="none") + theme(plot.title = element_text(hjust = 0.5)) +
-          geom_vline(xintercept = 0, color = "blue", size=0.5) + geom_vline(xintercept = -1, color = "red", size=0.5)
-        if (i==1){  
-          print(g)
-        } else {
-            print(g+geom_text_repel(angle=90, max.overlaps = Inf))
-          }
-        i<-i+1
-        }
+    pdf(paste0("CRISPRscores_",gene,".pdf"), width=12, height=20)
+    i<-1
+    for (df_design in list(df_designAll, df_designHemato)){
+      df_gene<- chronos[rownames(chronos)==gene,df_design$Sample]
+      df_gene<-as.data.frame(t(df_gene))
+      colnames(df_gene)<-"Gene"
+      df_gene$Group<-factor(df_design$Group, levels=unique(df_design$Group))
+      df_gene$color<- "black"
+      df_gene$Rep<-factor(df_design$Rep, levels=unique(df_design$Rep))
+      df_gene$Group<-interaction(df_gene$Group,df_gene$Rep, lex.order = T)
+      
+      # Barplot
+      g<-ggplot(df_gene, aes(x=Gene, y=Group, fill=Group, label=rownames(df_gene))) + geom_boxplot() + geom_jitter(color=df_gene$color, size=2, alpha=0.9, height = 0) + 
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + labs(y = "", x="CRISPR score (Chronos)") +
+        ggtitle(paste0(gene, " knockout (DepMap)")) + theme(legend.position="none") + theme(plot.title = element_text(hjust = 0.5)) +
+        geom_vline(xintercept = 0, color = "blue", size=0.5) + geom_vline(xintercept = -1, color = "red", size=0.5)
+      if (i==1){  
+        print(g)
+      } else {
+        print(g+geom_text_repel(angle=90, max.overlaps = Inf))
+      }
+      i<-i+1
+    }
     dev.off()
   }else{
     print(paste0(gene, " is not available"))
