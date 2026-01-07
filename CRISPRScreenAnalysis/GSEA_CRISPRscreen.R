30d09d48ec84cbc9a9011d41e4a017434bea35f2
Martin F.M. de Rooij, PhD <derooij_m@hotmail.com>
2026-01-07 16:07:25 +0100

    Add files via upload

--- CRISPRScreenAnalysis/GSEA_CRISPRscreen.R
+++ CRISPRScreenAnalysis/GSEA_CRISPRscreen.R
@@ -1,14 +1,13 @@
-# Use the CRISPRScreenAnalysis.R output files, put this script in the same folder, adjust the settings, and run the script in Rstudio.
-# This script performs gene set enrichment analysis using the ClusterProfiler package
+# Use the CRISPRScreenAnalysis.R output files, put this script in the same folder, add BrunelloHGNC.csv to this folder, adjust the settings, and run the script in Rstudio.
+# This script performs STRING analysis with STRINGdb, and gene set enrichment analysis with ClusterProfiler
 # Author: M.F.M. de Rooij PhD, Amsterdam UMC, Spaargaren Lab, 2023, info: m.f.derooij@amsterdamumc.nl
 ################################################################################
 ## Install package
 #install.packages("BiocManager")
-#BiocManager::install(c("clusterProfiler","msigdbr","enrichplot","org.Hs.eg.db","AnnotationDbi","ReactomePA","HGNChelper","pathview", "ggplot2"))
+#BiocManager::install(c("clusterProfiler","msigdbr","enrichplot","org.Hs.eg.db","AnnotationDbi","ReactomePA","HGNChelper","pathview", "ggplot2", "STRINGdb"))
 #install.packages("writexl")
 ################################################################################
-# 
-# Workdirectory:
+
 # Put this script in the folder where the files of  gene statistics are located
 folder<- dirname(rstudioapi::getActiveDocumentContext()$path)
 
@@ -27,19 +26,48 @@ library("HGNChelper")
 library("writexl")
 library("pathview")
 library("ggplot2")
+library("STRINGdb")
 
 setwd(folder)
-comparisons <- list.files(pattern="Genes.csv$")
+dir.create("GSEA_STRING")
 
+# Up to 1100 gene symbols are outdated in the Brunello annotation
+BrunelloHGNC<- read.csv("BrunelloHGNC.csv")
+
+comparisons <- list.files(pattern="Genes.csv$")
 for (com in comparisons) {
+  # Read data
   data <- read.csv(com)
   
-  # Data
-  Hugo <- checkGeneSymbols(data$GeneSymbol, unmapped.as.na=T)
+  # Adjust gene symbols to recent hugo symbols from file
+  data$GeneSymbolHGNC<- data$GeneSymbol
+  data$GeneSymbolHGNC[data$GeneSymbolHGNC %in% BrunelloHGNC$wrongGeneSymbol] <- 
+    BrunelloHGNC$goodGeneSymbol[match(data$GeneSymbolHGNC[data$GeneSymbolHGNC %in% BrunelloHGNC$wrongGeneSymbol], BrunelloHGNC$wrongGeneSymbol)]  
+  
+  # Check if it is still up to date
+  Hugo <- checkGeneSymbols(data$GeneSymbolHGNC, unmapped.as.na=T)
   data$Hugo <- Hugo$Suggested.Symbol
+  data$Hugo[is.na(data$Hugo)]<- data$GeneSymbolHGNC[is.na(data$Hugo)]
   data$EntrezID <- as.character(mapIds(org.Hs.eg.db, keys = data$Hugo, column = "ENTREZID", keytype = "SYMBOL"))
   data<- data[!is.na(data$EntrezID),]
   data<- data[data$EntrezID!="NULL",]
+  
+  # Remove duplicate Hugo symbols, take the one with highest median fold change if the original is not there
+  duplicates<- data.frame(h=data$Hugo, n=1)
+  duplicates<- aggregate(duplicates$n, by=list(duplicates$h), FUN=sum)
+  duplicates<- duplicates[duplicates$x>1,]
+  dataDupl<- data[data$Hugo %in% duplicates$Group.1,]
+  
+  data<- data[!data$Hugo %in% duplicates$Group.1,]
+  for (g in unique(dataDupl$Hugo)){
+    temp<- dataDupl[dataDupl$Hugo==g,]
+    idmax<- temp$GeneSymbol[temp$MedianFoldChange==max(abs(temp$MedianFoldChange))][1]
+    if (sum(temp$GeneSymbol==g)==1){
+      data<- rbind(data, dataDupl[dataDupl$GeneSymbol==g,])
+      } else{
+      data<- rbind(data, dataDupl[dataDupl$GeneSymbol==idmax,])
+    }
+  }
   
   # Gene list with gene symbols
   geneList<- log2(data$MedianFoldChange)
@@ -54,11 +82,46 @@ for (com in comparisons) {
   geneListID<- geneListID[order(geneListID, decreasing=T)]
   # To prevent a bug
   geneList[geneList==0]<- 10^-10
+
+  # STRING
+  data$logFC<- log2(data$MedianFoldChange)
+  data$fdr<- apply(data[,c("fdrDepleted","fdrEnriched")], 1, FUN=min)
   
+  hitsDown<- data$Hugo[data$fdrDepleted < 0.1]
+  hitsUp<- data$Hugo[data$fdrEnriched < 0.1]
+  hits<- c(hitsDown,hitsUp)
+  hitsDown50<- data$Hugo[order(data$MedianFoldChange)][1:50]
+  hitsUp50<- data$Hugo[order(data$MedianFoldChange, decreasing = T)][1:50]
+  
+  
+  string_db <- STRINGdb$new(version="12.0", species=9606, score_threshold=400, input_directory="")
+  example1_mapped = string_db$map(data, "Hugo", removeUnmappedRows = TRUE)
+  example1_mapped_fdr = string_db$add_diff_exp_color(subset(example1_mapped, fdr<0.1), logFcColStr="logFC")
+  payload_id = string_db$post_payload(example1_mapped_fdr$STRING_id, colors=example1_mapped_fdr$color)
+  
+  pdf(paste0("GSEA_STRING/STRING_",com,".pdf"),10,10)
+    if (length(hits)>0){
+      string_db$plot_network(hits, payload_id=payload_id)
+      title(main = "Hits All", adj = 0, cex.main = 1.5, col.main = "black")
+    }
+    if (length(hitsDown)>0){
+      string_db$plot_network(hitsDown, payload_id=payload_id)
+      title(main = "Hits Down", adj = 0, cex.main = 1.5, col.main = "black")
+    }
+    if (length(hitsUp)>0){
+      string_db$plot_network(hitsUp, payload_id=payload_id)
+      title(main = "Hits Up", adj = 0, cex.main = 1.5, col.main = "black")
+    }
+    string_db$plot_network(hitsDown50, payload_id=payload_id)
+    title(main = "Down 50", adj = 0, cex.main = 1.5, col.main = "black")
+    string_db$plot_network(hitsUp50, payload_id=payload_id)
+    title(main = "Up 50", adj = 0, cex.main = 1.5, col.main = "black")
+  dev.off()
+  
+  # GSEA
   sheets<- list()
-  
   set.seed(211081)
-  
+
   # KEGG
   cat<- "KEGG"
   sheets[[paste0("GSEA_",cat)]]<- vector()
@@ -81,7 +144,7 @@ for (com in comparisons) {
   }
 
   # MSIGDB
-  for (cat in c("H", "C2", "C1")) {
+  for (cat in c("H", "C1", "C2", "C3")) {
     sheets[[paste0("GSEA_MSIGDB_",cat)]]<- vector()
     sig <- msigdbr(species="Homo sapiens", category=cat)[,c("gs_name", "entrez_gene")]
     try(sheets[[paste0("GSEA_MSIGDB_",cat)]] <- as.data.frame(setReadable(GSEA(geneList=geneListID, TERM2GENE = sig, minGSSize=minGSSize, pvalueCutoff=pvalueCutoff, seed=T), 'org.Hs.eg.db', 'ENTREZID')@result))
@@ -93,35 +156,38 @@ for (com in comparisons) {
       sheets[[i]]<- data.frame(ID="No Matches")
     }
   }
-  write_xlsx(sheets, paste0("GSEA_ClusterProfiler_", com,".xlsx"))
+  write_xlsx(sheets, paste0("GSEA_STRING/GSEA_ClusterProfiler_", com,".xlsx"))
 
   GSEAplots <- setReadable(gseKEGG(geneList=geneListID, organism='hsa', minGSSize=minGSSize, pvalueCutoff=pvalueCutoff, seed=T), 'org.Hs.eg.db', 'ENTREZID')
   if (GSEAplots@setType=="KEGG"){
-    dir.create(paste0("GSEA_KEGGpathview_",com))
-    setwd(paste0("GSEA_KEGGpathview_",com))
+    dir.create(paste0("GSEA_STRING/GSEA_KEGGpathview_",com))
+    setwd(paste0("GSEA_STRING/GSEA_KEGGpathview_",com))
     for (i in 1:20){
       try(pathview(gene.data  = geneListID,
                    pathway.id = GSEAplots$ID[i],
                    species    = "hsa"))
     }
-    setwd("../")
+    setwd("../../")
   }
-  pdf(paste0("GSEA_",GSEAplots@setType,"_", com,".pdf"), 13,10)
-  
-    print(dotplot(GSEAplots, showCategory=20) + ggtitle("Dotplot")+xlab("Gene ratio (# leading egde genes / # available genes)"))
-    
+
+  pdf(paste0("GSEA_STRING/GSEA_",GSEAplots@setType,"_", com,".pdf"), 10,7)
+
+    print(dotplot(GSEAplots, x = "NES", color = "p.adjust", size = "GeneRatio", orderBy = "NES", showCategory = 10, split = ".sign") +
+            ggtitle("Dotplot") + theme(aspect.ratio = 2)#+ facet_grid(.~.sign)
+    )
+
+    print(ridgeplot(GSEAplots, showCategory=20)+ggtitle("Ridgeplot")+xlab("Log2 fold change")+ theme(aspect.ratio = 2))
+
     print(treeplot(pairwise_termsim(GSEAplots), cluster.params = list(method = "average"))+ggtitle("Treeplot"))
-    
+
     print(emapplot(pairwise_termsim(GSEAplots))+ggtitle("Enrichment map"))
-    
+
     print(cnetplot(GSEAplots, color.params=list(foldChange=geneList, edge=T))+ggtitle("CNET plot"))
-    
-    print(ridgeplot(GSEAplots, showCategory=20)+ggtitle("Ridgeplot")+xlab("Log2 fold change"))
-    
+
     print(heatplot(GSEAplots, foldChange=geneList, showCategory=20)+ggtitle("Heatplot"))
-    
+
     for (i in 1:20){
-      print(gseaplot2(GSEAplots, geneSetID = i, title = paste0(com, ": ",GSEAplots$Description[i], " (",GSEAplots@setType,":", GSEAplots$ID[i],")", 
+      print(gseaplot2(GSEAplots, geneSetID = i, title = paste0(com, ": ",GSEAplots$Description[i], " (",GSEAplots@setType,":", GSEAplots$ID[i],")",
                                                                "\nES: ", round(GSEAplots$enrichmentScore[i],3), ",",
                                                                " NES: ", round(GSEAplots$NES[i],3), ",",
                                                                " pvalue: ",if (GSEAplots$pvalue[i]>= 0.001) {round(GSEAplots$pvalue[i],3)}
@@ -130,5 +196,6 @@ for (com in comparisons) {
                                                                else{formatC(GSEAplots$p.adjust[i],format = "e", digits = 2)},",",
                                                                " leading edge: (", GSEAplots$leading_edge[i],")" )))
     }
+
     dev.off()
 }
